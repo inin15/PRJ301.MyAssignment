d64c043c7a97ca8edfa1e6312106ba454d188382
inin15 <101159760+inin15@users.noreply.github.com>
2022-06-21 01:07:59 +0700

    mockup screen + attendance
    mockup screen + attendance

--- MyProject/src/java/dal/StuDBContext.java
+++ MyProject/src/java/dal/StuDBContext.java
@@ -8,223 +8,38 @@
 package dal;
 
 import java.util.ArrayList;
+import model.Students;
 
 /**
  *
  * @author Adim
  */
-public class StuDBContext extends DBContext<Deprecated> {
+public class StuDBContext extends DBContext<Students> {
 
     @Override
-    public ArrayList<Deprecated> list() {
+    public ArrayList<Students> list() {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
     @Override
-    public Deprecated get(int id) {
+    public Students get(int id) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
     @Override
-    public void insert(Deprecated model) {
+    public void insert(Students model) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
     @Override
-    public void update(Deprecated model) {
+    public void update(Students model) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
     @Override
-    public void delete(Deprecated model) {
+    public void delete(Students model) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
-  /**  public ArrayList<Stu> searchByDid(int did) {
-        ArrayList<Stu> stu = new ArrayList<>();
-        try {
-            String sql = "SELECT e.eid,e.ename,e.gender,e.dob,d.did,d.dname\n"
-                    + "FROM Emp e INNER JOIN Dept d\n"
-                    + "ON e.did = d.did\n"
-                    + "WHERE d.did = ?";
-            PreparedStatement stm = connection.prepareStatement(sql);
-            stm.setInt(1, did);
-            ResultSet rs = stm.executeQuery();
-            while (rs.next()) {
-                Dept d = new Dept();
-                d.setId(rs.getInt("did"));
-                d.setName(rs.getString("dname"));
-                Stu e = new Stu();
-                e.setId(rs.getInt("eid"));
-                e.setName(rs.getString("ename"));
-                e.setGender(rs.getBoolean("gender"));
-                e.setDob(rs.getDate("dob"));
-                e.setDept(d);
-                stu.add(e);
-            }
-        } catch (SQLException ex) {
-            Logger.getLogger(StuDBContext.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return stu;
-    }
-
-    public ArrayList<Stu> filter(Integer id, String name, Boolean gender, Date from, Date to, Integer did) {
-        ArrayList<Stu> stu = new ArrayList<>();
-        try {
-            String sql = "SELECT e.eid,e.ename,e.gender,e.dob,d.did,d.dname \n"
-                    + "FROM Emp e INNER JOIN Dept d ON e.did = d.did\n"
-                    + "WHERE (1=1)";
-            Integer index = 0;
-            HashMap<Integer, Object> params = new HashMap<>();
-            if (id != null) {
-                index++;
-                sql += " AND e.eid = ?";
-                params.put(index, id);
-            }
-
-            if (name != null) {
-                index++;
-                sql += " AND e.ename like '%' + ? + '%'";
-                params.put(index, name);
-            }
-
-            if (gender != null) {
-                index++;
-                sql += "AND e.gender = ?";
-                params.put(index, gender);
-            }
-
-            if (from != null) {
-                index++;
-                sql += " AND e.dob >= ?";
-                params.put(index, from);
-            }
-
-            if (to != null) {
-                index++;
-                sql += " AND e.dob <= ?";
-                params.put(index, to);
-            }
-
-            if (did != null) {
-                index++;
-                sql += " AND e.did = ?";
-                params.put(index, did);
-            }
-            PreparedStatement stm = connection.prepareStatement(sql);
-            for (Map.Entry<Integer, Object> entry : params.entrySet()) {
-                Integer key = entry.getKey();
-                Object val = entry.getValue();
-                stm.setObject(key, val);
-            }
-            ResultSet rs = stm.executeQuery();
-            while (rs.next()) {
-                Dept d = new Dept();
-                d.setId(rs.getInt("did"));
-                d.setName(rs.getString("dname"));
-                Stu e = new Stu();
-                e.setId(rs.getInt("eid"));
-                e.setName(rs.getString("ename"));
-                e.setGender(rs.getBoolean("gender"));
-                e.setDob(rs.getDate("dob"));
-                e.setDept(d);
-                stu.add(e);
-            }
-        } catch (SQLException ex) {
-            Logger.getLogger(StuDBContext.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return stu;
-    }
-
-    @Override
-    public ArrayList<Stu> list() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    @Override
-    public Stu get(int id) {
-        try {
-            String sql = "SELECT e.eid,e.ename,e.gender,e.dob,d.did,d.dname\n"
-                    + "FROM Emp e INNER JOIN Dept d\n"
-                    + "ON e.did = d.did\n"
-                    + "WHERE e.eid = ?";
-            PreparedStatement stm = connection.prepareStatement(sql);
-            stm.setInt(1, id);
-            ResultSet rs = stm.executeQuery();
-            while (rs.next()) {
-                Dept d = new Dept();
-                d.setId(rs.getInt("did"));
-                d.setName(rs.getString("dname"));
-                Stu e = new Stu();
-                e.setId(rs.getInt("eid"));
-                e.setName(rs.getString("ename"));
-                e.setGender(rs.getBoolean("gender"));
-                e.setDob(rs.getDate("dob"));
-                e.setDept(d);
-                return e;
-            }
-        } catch (SQLException ex) {
-            Logger.getLogger(StuDBContext.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return null;
-    }
-
-    @Override
-    public void insert(Stu model) {
-        try {
-            String sql = "INSERT INTO [Emp]\n"
-                    + "           ([ename]\n"
-                    + "           ,[dob]\n"
-                    + "           ,[gender]\n"
-                    + "           ,[did])\n"
-                    + "     VALUES\n"
-                    + "           (?\n"
-                    + "           ,?\n"
-                    + "           ,?\n"
-                    + "           ,?)";
-            PreparedStatement stm = connection.prepareStatement(sql);
-            stm.setString(1, model.getName());
-            stm.setDate(2, model.getDob());
-            stm.setBoolean(3, model.isGender());
-            stm.setInt(4, model.getDept().getId());
-            stm.executeUpdate();
-        } catch (SQLException ex) {
-            Logger.getLogger(StuDBContext.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-
-    @Override
-    public void update(Stu model) {
-        try {
-            String sql = "UPDATE [Emp]\n"
-                    + "   SET [ename] = ?\n"
-                    + "      ,[dob] = ?\n"
-                    + "      ,[gender] = ?\n"
-                    + "      ,[did] = ?\n"
-                    + " WHERE [eid] = ?";
-            PreparedStatement stm = connection.prepareStatement(sql);
-            stm.setString(1, model.getName());
-            stm.setDate(2, model.getDob());
-            stm.setBoolean(3, model.isGender());
-            stm.setInt(4, model.getDept().getId());
-            stm.setInt(5, model.getId());
-            stm.executeUpdate();
-        } catch (SQLException ex) {
-            Logger.getLogger(StuDBContext.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-
-    @Override
-    public void delete(Stu model) {
-        try {
-            String sql = "DELETE Emp"
-                    + " WHERE [eid] = ?";
-            PreparedStatement stm = connection.prepareStatement(sql);
-            stm.setInt(1, model.getId());
-            stm.executeUpdate();
-        } catch (SQLException ex) {
-            Logger.getLogger(StuDBContext.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-*/
+   
 }
